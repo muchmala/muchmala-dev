0cdf0712ea3c7da8a82b181a551f4d1d60819e3e
George Miroshnykov <george.miroshnykov@gmail.com>
2011-06-26 16:05:18 +0300

    Moved environment config from user's home to /etc/profile.d/muchmala.sh.

--- chef/cookbooks/muchmala/muchmala/recipes/env.rb
+++ chef/cookbooks/muchmala/muchmala/recipes/env.rb
@@ -1,13 +1,4 @@
-template "/home/#{node[:muchmala][:user]}/.profile" do
-    source "profile.env"
-    mode 0644
-    owner "#{node[:muchmala][:user]}"
-    group "#{node[:muchmala][:group]}"
-end
-
-template "/home/#{node[:muchmala][:user]}/.muchmala" do
+template "/etc/profile.d/muchmala.sh" do
     source "muchmala.env"
     mode 0644
-    owner "#{node[:muchmala][:user]}"
-    group "#{node[:muchmala][:group]}"
 end
